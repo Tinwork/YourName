edf79fb216c8a7ceae765b0c76a4ddc085c3d045
Marc Intha-amnouay <marc.inthaamnouay@gmail.com>
2017-12-05 00:50:25 +0100

    Add toolbar.
    Sometimes i hate myself.

--- app/src/main/java/com/yellowman/tinwork/yourname/home/HomeActivity.java
+++ app/src/main/java/com/yellowman/tinwork/yourname/home/HomeActivity.java
@@ -3,22 +3,61 @@ package com.yellowman.tinwork.yourname.home;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.yellowman.tinwork.yourname.R;
+import com.yellowman.tinwork.yourname.UIKit.GradientGenerator;
 import com.yellowman.tinwork.yourname.login.LoginActivity;
 import com.yellowman.tinwork.yourname.networkTest.NetworkActivity;
 import com.yellowman.tinwork.yourname.utils.Utils;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private GradientGenerator gd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        setToolbar();
+        initView();
         isUserSubscribe();
         launchActivity();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Init View
+     */
+    private void initView() {
+        LinearLayout mainLayout = (LinearLayout) findViewById(R.id.mainLayout);
+        this.gd = new GradientGenerator(this, mainLayout);
+        this.gd.buildBackgroundGradientColor();
+    }
+
+    /**
+     * Set ToolBar
+     */
+    private void setToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.mytoolbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
