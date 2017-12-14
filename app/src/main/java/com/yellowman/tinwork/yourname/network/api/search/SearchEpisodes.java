ac65d4e45ed2d09348438984065bc4cb40ccd918
Antoine Renault <antoine.renault.mmi@gmail.com>
2017-12-14 10:34:06 +0100

    Add: WIP SearchEpisodes

---
+++ app/src/main/java/com/yellowman/tinwork/yourname/network/api/search/SearchEpisodes.java
@@ -0,0 +1,52 @@
+package com.yellowman.tinwork.yourname.network.api.search;
+
+import android.content.Context;
+
+import com.yellowman.tinwork.yourname.model.Search;
+import com.yellowman.tinwork.yourname.network.Listeners.GsonCallback;
+import com.yellowman.tinwork.yourname.network.api.Routes;
+import com.yellowman.tinwork.yourname.network.fetch.Fetch;
+import com.yellowman.tinwork.yourname.network.fetch.GsonGetManager;
+import com.yellowman.tinwork.yourname.network.fetch.RequestQueueManager;
+import com.yellowman.tinwork.yourname.utils.Utils;
+
+import java.util.HashMap;
+
+/**
+ * Created by aktane on 14/12/17.
+ */
+
+public class SearchEpisodes extends Fetch {
+    private final RequestQueueManager queueManager;
+    private Context ctx;
+    private GsonGetManager<Search> series;
+    private int retry;
+
+    /**
+     *
+     * @param context
+     */
+    public SearchEpisodes(Context context) {
+        this.ctx = context;
+        this.queueManager = RequestQueueManager.getInstance(this.ctx.getApplicationContext());
+        this.retry = 0;
+    }
+
+    @Override
+    public void get(HashMap<String, String> payload, final GsonCallback callback) {
+        String token = Utils.getSharedPreference(ctx, "yourname_token");
+        // Headers
+        HashMap<String, String> headers = Utils.makeHeaders(null, token);
+        // Bind the GET request params
+        String URL = Utils.buildGetUrl(Routes.SEARCH_EPISODES, payload);
+
+        series = new GsonGetManager<>(URL, Search.class, headers, response -> {
+            callback.onSuccess(response);
+        }, error -> {
+            this.handleVolleyError(error, series, ctx, retry, callback);
+            retry++;
+        });
+
+        queueManager.addToRequestQueue(series);
+    }
+}
